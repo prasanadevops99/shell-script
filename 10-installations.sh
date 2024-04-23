e16c0268ac9dca4bb919d867c6caa9a2f4fc859f
prasanadevops99 <prasanadevops99@gmail.com>
2024-04-23 17:35:44 +0530

    hello script

--- 10-installations.sh
+++ 10-installations.sh
@@ -4,10 +4,12 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "plese run this cript with root access."
+    echo "plese run this script with root access."
 else
 echo "you are super user"
 
 fi
 dnf install mysql -y
+
+echo "is script proceeding?"
 
