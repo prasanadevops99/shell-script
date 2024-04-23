30cf099aa2cc93d35456f92898e29aa253dad78e
prasanadevops99 <prasanadevops99@gmail.com>
2024-04-23 17:03:27 +0530

    hello script

--- 10-installations.sh
+++ 10-installations.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
+#!bin/bash
 
-
-USERID=$(id -u)
-
-if [ $ID -ne 0]
-then
-    echo "Please run this script with root access"
-else
-     echo "super user"
-
-fi
 dnf install mysql -y
-
